25a4e6dc2badc1fedccad713aeaa58442080b7f6
razorbacks-coder <medesano.razorbacks@gmail.com>
2026-03-18 20:51:44 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,25 +1,4 @@
 const TelegramBot = require('node-telegram-bot-api');
-
-const token = process.env.BOT_TOKEN;
-
-const bot = new TelegramBot(token, { polling: true });
-
-bot.onText(/\/start/, (msg) => {
-  bot.sendMessage(msg.chat.id, "🏈 1st & Bot online!");
-});
-
-bot.onText(/\/rules/, (msg) => {
-  bot.sendMessage(msg.chat.id, "Chiedi pure qualcosa sul regolamento della lega.");
-});
-
-bot.on('message', (msg) => {
-  if (msg.text.startsWith("/")) return;
-
-  bot.sendMessage(msg.chat.id, "🤖 Sto ancora imparando, presto risponderò alle domande della lega!");
-});
-
-
-const TelegramBot = require('node-telegram-bot-api');
 const axios = require('axios');
 
 const token = process.env.BOT_TOKEN;
